0e0de9635fed9bc6ce051dd956c4365db141f9a2
NemesisUA <display.n0ne.01@gmail.com>
2020-10-02 15:45:59 +0300

    feat: add function min, max, avr

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let summ = 0;
+    for (let i = 0; i < array.length; i++) {
+      summ += array[i];
+      
+    } return summ / array.length;
+  }
+  
 }
